17aebc1ef1940b94a89084150846c2047b1fcb27
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-09 15:04:39 +0100

    Comments.

--- js/components/clock.js
+++ js/components/clock.js
@@ -6,6 +6,7 @@ import data from '../structure';
 
 let clockRef = data.reference('clock');
 
+// The forceUpdateOn mixin will forceUpdate the component every time the clock reference changes.
 export default component('Clock', forceUpdateOn(clockRef), () =>
   <span>
     {clockRef.cursor().deref()}
