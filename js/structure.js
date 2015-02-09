1cbcd83db7d11a4c6741a6de35fb61e0cbce01ab
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-09 15:02:47 +0100

    English.

--- js/structure.js
+++ js/structure.js
@@ -7,7 +7,7 @@ let data = immstruct({
 
 export default data;
 
-// Tick the structures clock every second.
+// Tick the structure's clock every second.
 setInterval(
   () => data.cursor('clock').update(_ => String(new Date())),
   1000);
