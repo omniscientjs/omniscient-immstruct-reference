5747393ab8a7b8ddda27eac7171caa1c784ffbaf
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-09 14:59:12 +0100

    Cleans up the gh-pages branch.

--- js/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import component from 'omniscient';
-
-import App from './components/app';
-import data from './structure';
-
-React.render(<App header={data.cursor('header')} />, document.body);
